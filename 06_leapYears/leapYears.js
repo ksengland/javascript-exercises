ea7c8fc9a2492c64ece72c18607ab5ea34561896
ksengland <karleenengland@runbox.com>
2022-03-20 17:02:04 -0500

    completed 4-11

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+//is year / 4? && is year not / 100? = leap
+//is year / 4? && is year / 100? &&  year/ 400? = leap
+//if not = not leap
+    return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0)
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    reverse =  string.toLowerCase().replace(/[^A-Za-z]/g, "");
+return (
+    reverse
+    .split("")
+    .reverse()
+    .join("") == reverse
+);
 };
 
 // Do not edit below this line
