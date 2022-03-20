af3e4a7cd32b84a1d0acffe1532860963896bce4
ksengland <karleenengland@runbox.com>
2022-03-20 11:45:10 -0500

    completed 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(greeting, times) {
+if (times < 0) return 'ERROR'
+let string = ''
+for (let i = 0; i < times; i++) {
+        string += greeting;
+}
+ return string   
 };
-
 // Do not edit below this line
 module.exports = repeatString;
